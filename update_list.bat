42f975f41797fcef054dee6be1aedeb9ac712cc8
Elifian <69606089+elifian@users.noreply.github.com>
2024-10-12 15:11:29 +0500

    update .bat file

--- update_list.bat
+++ update_list.bat
@@ -1,2 +1,3 @@
 @echo off
 curl -o "lists\list.txt" https://antifilter.download/list/domains.lst
+curl -o "lists\list-additional.txt" https://raw.githubusercontent.com/elifian/zapret-bypass/refs/heads/main/lists/list-additional.txt
