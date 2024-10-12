44ca1c33a1dac87d7fbec8dd68c65fe8fe1883d1
Elifian <69606089+elifian@users.noreply.github.com>
2024-10-12 15:51:46 +0500

    update .bat files

--- service.bat
+++ service.bat
@@ -1,5 +1,5 @@
 @echo off
 %~dp0service install > NUL 2>&1
 sc start zapret > NUL 2>&1
-sc query zapret | find "RUNNING" >nul && echo SERVICE ZAPRET INSTALLED || echo SERVICE NOT INSTALLED
-sleep 2
+sc query zapret | find "PENDING" >nul && echo SERVICE ZAPRET INSTALLED || echo SERVICE NOT INSTALLED
+timeout /t 1 /nobreak
