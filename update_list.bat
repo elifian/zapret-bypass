e47ceb8e45e0d3cc54c9a4e78c09820f14161c9c
elifian <vasilyvaganovv@gmail.com>
2024-11-12 11:34:27 +0500

    3.1

--- update_list.bat
+++ update_list.bat
@@ -1,6 +1,5 @@
 @echo off
 
 curl -o "lists\list.txt" https://antifilter.download/list/domains.lst
-curl -o "lists\list-additional.txt" https://raw.githubusercontent.com/elifian/zapret-bypass/refs/heads/main/lists/list-additional.txt
 sc query zapret | findstr "RUNNING PENDING" >nul && sc stop zapret >nul & timeout /t 2 & sc start zapret >nul
 exit
