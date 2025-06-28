fdb5209a268a45d4ec55d5d203d60a0094f34df5
Elifian <vasilyvaganovv@gmail.com>
2025-06-28 14:32:24 +0500

    5.0.4

--- service_3.bat
+++ service_3.bat
@@ -3,10 +3,13 @@ echo Please, wait...
 
 set lockfile=%TEMP%\batchlock.txt
 if exist "%lockfile%" (
+    powershell -Command "Add-Type -AssemblyName 'Microsoft.VisualBasic'; [Microsoft.VisualBasic.Interaction]::MsgBox('Service install is running!', '16', 'Zapret')"
     exit /b
 ) else (
     echo 1 > "%lockfile%"
 )
+
+cd %~dp0
 
 reg query "HKU\S-1-5-19\Environment" >nul 2>&1
 if "%Errorlevel%" NEQ "0" (
@@ -15,17 +18,17 @@ if "%Errorlevel%" NEQ "0" (
     exit /b
 )
 
-set "file=%~dp0zapret.xml"
-set "Arguments=--wf-tcp=80,443 --wf-udp=443,50000-50100 --filter-udp=443 --hostlist-exclude=lists\list-exclude.txt --ipset=lists\list-ipset.txt --hostlist=lists\list.txt --hostlist=lists\list-additional.txt --hostlist-auto=lists\autohostlist.txt --hostlist-auto-fail-time=3 --hostlist-auto-fail-threshold=1 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=fake\quic_google.bin --new --filter-udp=50000-50100 --dpi-desync=fake,disorder2 --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new --filter-tcp=80 --hostlist-exclude=lists\list-exclude.txt --ipset=lists\list-ipset.txt --hostlist=lists\list.txt --hostlist=lists\list-additional.txt --hostlist-auto=lists\autohostlist.txt --hostlist-auto-fail-time=3 --hostlist-auto-fail-threshold=1 dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new --filter-tcp=443 --hostlist-exclude=lists\list-exclude.txt --ipset=lists\list-ipset.txt --hostlist=lists\list.txt -- --hostlist=lists\list-additional.txt --hostlist-auto=lists\autohostlist.txt --hostlist-auto-fail-time=3 --hostlist-auto-fail-threshold=1 --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-autottl --dpi-desync-fooling=badseq --dpi-desync-repeats=8"
+set "zapret=%~dp0zapret.xml"
+set "general=%~dp0general_3.bat"
 
-powershell -Command "(Get-Content '%file%') -replace '(?<=<arguments>).*?(?=</arguments>)', '%Arguments%' | Set-Content '%file%'" >nul 2>&1
+powershell -Command "$lines=Get-Content '%general%'; $commandLines=@(); $collecting=$false; foreach($line in $lines){if($line -match '^start\s+\""zapret\""'){$collecting=$true};if($collecting){$commandLines+=$line -replace '\s*\^$',''};if($collecting -and -not ($line -match '\s*\^$')){break}};$command=$commandLines -join ' '; $Arguments=''; if($command -match '\""%%~dp0bin\\winws\.exe\""(\s+(.+))?'){$Arguments=$matches[2];if($Arguments){$Arguments=$Arguments -replace '\""%%~dp0([^\""]+)\""','$1'}}; [xml]$xml=Get-Content '%zapret%'; $xml.service.arguments=$Arguments.Trim(); $xml.Save('%zapret%')"
 
 sc query zapret >nul 2>&1
 if %errorlevel%==0 (
     sc stop zapret >nul
     sc stop windivert >nul
     sc delete zapret >nul
-    powershell -Command "Start-Sleep -Milliseconds 1100" 
+    powershell -Command "Start-Sleep -Milliseconds 1100" >nul
 )
 
 "%~dp0zapret.exe" install >nul
