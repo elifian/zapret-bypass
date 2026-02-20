432ddd1745c05523ff98e6c2b38812235f0a2fa7
Elifian <vasilyvaganovv@gmail.com>
2026-02-20 17:23:09 +0500

    6.0.0

--- service_1.bat
+++ service_1.bat
@@ -14,14 +14,14 @@ cd %~dp0
 reg query "HKU\S-1-5-19\Environment" >nul 2>&1
 if "%Errorlevel%" NEQ "0" (
     PowerShell.exe -WindowStyle Hidden -NoProfile -NoLogo -Command ^
-    "Start-Process -Verb RunAs -FilePath '%~f0' -ArgumentList '%*'" 
+    "Start-Process -Verb RunAs -FilePath '%~f0' -ArgumentList '%*'"
     exit /b
 )
 
-set "zapret=%~dp0zapret.xml"
+set "zapret=%~dp0winsw.xml"
 set "general=%~dp0general_1.bat"
 
-powershell -Command "$lines=Get-Content '%general%'; $commandLines=@(); $collecting=$false; foreach($line in $lines){if($line -match '^start\s+\""zapret\""'){$collecting=$true};if($collecting){$commandLines+=$line -replace '\s*\^$',''};if($collecting -and -not ($line -match '\s*\^$')){break}};$command=$commandLines -join ' '; $Arguments=''; if($command -match '\""%%~dp0bin\\winws\.exe\""(\s+(.+))?'){$Arguments=$matches[2];if($Arguments){$Arguments=$Arguments -replace '\""%%~dp0([^\""]+)\""','$1'}}; [xml]$xml=Get-Content '%zapret%'; $xml.service.arguments=$Arguments.Trim(); $xml.Save('%zapret%')"
+powershell -Command "$lines=Get-Content '%general%'; $commandLines=@(); $collecting=$false; foreach($line in $lines){if($line -match '^start\s+\""zapret\""'){$collecting=$true}; if($collecting){if($line -match '^\s*$'){continue}; $cleanLine=$line -replace '\s*\^\s*$',''; $commandLines+=$cleanLine; if($line -notmatch '\^\s*$'){break}}}; $command=$commandLines -join ' '; $Arguments=''; if($command -match '\""%%~dp0bin\\winws\.exe\""(\s+(.+))?'){$Arguments=$matches[2];if($Arguments){$Arguments=$Arguments -replace '\""%%~dp0([^\""]+)\""','$1'}}; [xml]$xml=Get-Content '%zapret%'; $xml.service.arguments=$Arguments.Trim(); $xml.Save('%zapret%')"
 
 sc query zapret >nul 2>&1
 if %errorlevel%==0 (
@@ -31,7 +31,7 @@ if %errorlevel%==0 (
     powershell -Command "Start-Sleep -Milliseconds 1100" >nul
 )
 
-"%~dp0zapret.exe" install >nul
+"%~dp0winsw.exe" install >nul
 sc start zapret >nul
 
 for /f "tokens=3" %%i in ('sc query zapret ^| findstr /i "RUNNING START_PENDING"') do set SERVICE_STATUS=%%i
